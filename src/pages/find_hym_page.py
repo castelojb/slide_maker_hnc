8c11220f0302449839f99f1b0be23857cba44216
castelojb <joaocb14@gmail.com>
2022-10-02 20:00:16 -0300

    fixing copy button

--- src/pages/find_hym_page.py
+++ src/pages/find_hym_page.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pyperclip
 
 from src.hymn.utils import get_letter, shown_search_bar
 from src.setup import base_path
@@ -10,7 +9,4 @@ def quick_hymn_show_page():
 	hymn = shown_search_bar(base_path)
 
 	letter = get_letter(hymn[1])
-	st.text(letter)
-
-	if st.button('Copiar', on_click=pyperclip.copy(letter)):
-		st.success('Hino copiado!', icon="✅")
+	st.code(letter, language='textile')
