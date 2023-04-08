f5e09f669a6f922a2fe4429455c41320c6322ba2
castelojb <joaocb14@gmail.com>
2023-04-08 17:10:24 -0300

    chore: initial commit for slides_maker backend

--- main.py
+++ main.py
@@ -1,32 +1,22 @@
-import streamlit as st
-from src.pages.find_hym_page import quick_hymn_show_page
-from src.pages.slide_maker_page import slide_maker_page
+import uvicorn
+from dotenv import load_dotenv
+from fastapi_pagination import add_pagination
+
+load_dotenv()
+
+from setup import app
+from server.hymns.controllers.HymnController import hymn_router
 
 
-def about_page():
+@app.get("/")
+async def root():
+	return {"message": "I'm alive"}
 
-    st.markdown('Desenvolvido por João Araújo Castelo Branco para fins didaticos')
-    st.markdown('Email: joaocb14@gmail.com')
 
+app.include_router(hymn_router)
+
+# add_pagination(app)
 
 if __name__ == '__main__':
-
-    st.title('Slide Maker')
-    st.subheader('Seu site para agilizar os slides')
-
-    options = [
-        ('Sobre', about_page),
-        # ('Busca Rápida', quick_hymn_show_page),
-        ('Slide Maker', slide_maker_page)
-    ]
-
-    with st.sidebar:
-        option = st.radio(
-            'Escolha o setor',
-            options,
-            format_func=lambda x: x[0]
-        )
-
-    st.subheader(option[0])
-    option[1]()
-
+	# keep this
+	uvicorn.run(app, host="0.0.0.0", port=8080)
